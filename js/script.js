45cb4ecf92d35bf6712a0728a1d98892f4e40ac6
Morris <1216770@student.roc-nijmegen.nl>
2025-11-24 08:42:23 +0100

    Update project link text for clarity in OVOOP description

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@ function describeProject(project) {
   if (currentProject == "ovoop") {
     targetedParagraphTitle.innerHTML = "OVOOP";
     targetedParagraphLink.href = "https://github.com/m3v64/ovOOP";
-    targetedParagraphLink.innerHTML = "Click here to get sent to the project!";
+    targetedParagraphLink.innerHTML = "Click here to get sent to the project's repository!";
     targetedParagraph.innerHTML =
       "The OVOOP project is a large public transport program that was coded using java. This is the first time i went further. In Mastermind it was basic code, here it got much more complicated, using the google library called GSON and using OOP (Object Oriented Programming) to make a fully working system with features such as an account system, a game version of it, a display map showing what route you took, a balance system, a travelling system and so much more.";
   }
